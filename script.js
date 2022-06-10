6838461ee0c48653d73e1aeed96a3b980f69e3cf
leetecea <leeleticiac@gmail.com>
2022-06-09 21:21:19 -0300

    primeira atividade

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Pimeira aula de Lógica com Javascript");
+
+var nomeCompleto = "Letícia Menezes"; //string -> texto
+console.log(nomeCompleto);
+
+const cpf = 255659; //number 
+console.log(cpf);
+
+const turmaUnida = true; //booleano
+console.log(typeof turmaUnida);
+
+var numero = null; //Null
+console.log(typeof numero);
+
+let passagem; // undefined -> esqueceu de definir o valor da variável
+console.log(passagem);
+
+let aluno1 = "Fulano"; // NaN -> Not a number
+let aluno2 = "Ciclano";
+console.log(aluno1 - aluno2)
+
+console.log(`${aluno1} ${aluno2}`) //Concatenação
