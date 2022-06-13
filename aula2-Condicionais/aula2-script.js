58a52748e06dba6c379b5a2b8b387f92374553c2
leetecea <leeleticiac@gmail.com>
2022-06-13 08:38:23 -0300

    Desafio 2 - if/else

---
+++ aula2-Condicionais/aula2-script.js
@@ -0,0 +1,46 @@
+
+let possuiGraduacao = false;
+
+if(possuiGraduacao){
+  console.log("Possui faculdade");
+} else{
+  console.log("Ainda não possui faculdade");
+}
+
+if(true){
+  console.log("É verdadeiro");
+}else{
+  console.log("É falso");
+}
+
+// let idade = 17;
+
+// if(idade >= 18){
+//   console.log("Pode entrar na festa");
+// }else{
+//   console.log("Não pode entrar na festa!");
+// }
+
+
+let maiorIdade = true;
+let estudandoProgramacao = true;
+
+if(maiorIdade){
+  console.log("É maior de idade");
+}else if(estudandoProgramacao){
+  console.log("Estudando programação");
+}else{
+  console.log("Foi tudo falso");
+}
+
+
+let idade = 18;
+let corRoupa = "Branca";
+
+if(idade >= 18){
+  console.log("É maior de idade, então pode entrar na festa");
+}else if(corRoupa == "Preta"){
+  console.log("Está de preto, então pode entrar");
+}else{
+  console.log("Po, não vai entrar hoje!");
+}
