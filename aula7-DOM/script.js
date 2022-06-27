0fa6b5ea247c07d8101309d3e5ec2259dfd8bbb2
leetecea <leeleticiac@gmail.com>
2022-06-27 10:33:05 -0300

    aula 7 - DOM

--- aula7-DOM/script.js
+++ aula7-DOM/script.js
@@ -10,7 +10,7 @@ let nome = window.prompt("Qual é o seu nome?")
 window.alert(`Bem vinda a sua primeira aula com DOM, ${nome}!!`)
 
 console.log("Aqui é o Window", window); //objeto global
-console.log("Aqui é o Document", document); //é o html
+console.log("Aqui é o Document", document); //é todo o html
 
 console.log(document.body.style);
 
